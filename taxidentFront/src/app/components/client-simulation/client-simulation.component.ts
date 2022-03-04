0f7f06f177454cabb0c0990332f59ef4609838ea
leagrdl <garandel.lea.lag@gmail.com>
2022-03-04 14:53:55 +0100

    simmulation

--- taxidentFront/src/app/components/client-simulation/client-simulation.component.ts
+++ taxidentFront/src/app/components/client-simulation/client-simulation.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-simulation.component.css']
 })
 export class ClientSimulationComponent implements OnInit {
+  date : any;
+  villeDep : any;
+  villeArr : any;
+  distance : any;
+  prix : any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  calculerTrajet(){
+    this.prix = (this.distance *(5/100*1.8 + 0.1 + 2)).toFixed(2); //conso 5 L/100 + 1.8 €/L + 10ct/km pour peage + 2€/km pour chauffeur
+  }
 }
